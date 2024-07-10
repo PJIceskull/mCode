bfaf26a28a5340d40b4a2326de48d94ce9f0f1c8
Pierce <Pierce@PIERCE>
2024-07-10 10:33:46 -0400

    created People Page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
 // Import Functions
 import { changeRoute } from "./model.js";
 
-// Variables
-let data = []; // Array to hold json Data
+// Global Variables
+let peopleData = []; // Array to hold json Data
+
+// Load data for Employees on People Page
+export function loadPeopleData() {
+  $.each(peopleData.Team_Members, function (index, teamMember) {
+    if (teamMember.Team == "Project") {
+      $("#Project_Team").append(`<section>
+        <div class="profileSection">
+          <div class="profileIMG">
+            <img
+              src="images/Profile/${teamMember.ProfileIMG}"
+              alt="Picture of Employee"
+              loading="lazy"
+            />
+          </div>
+          <div class="profileText">
+            <h3>${teamMember.name}</h3>
+            <p>${teamMember.Position}</p>
+          </div>
+        </div>
+        <div class="descriptionText">
+          <p>${teamMember.Description}</p>
+        </div>
+      </section>`);
+    } else if (teamMember.Team == "Advisory") {
+      $("#Advisory_Team").append(`<section>
+        <div class="profileSection">
+          <div class="profileIMG">
+            <img
+              src="images/Profile/${teamMember.ProfileIMG}"
+              alt="Picture of Employee"
+              loading="lazy"
+            />
+          </div>
+          <div class="profileText">
+            <h3>${teamMember.name}</h3>
+            <p>${teamMember.Position}</p>
+          </div>
+        </div>
+        <div class="descriptionText">
+          <p>${teamMember.Description}</p>
+        </div>
+      </section>`);
+    }
+  });
+}
+function getPeopleData() {
+  $.get(`data/projectTeam.json`, function (data) {
+    // Assign JSON data to global variable
+    peopleData = data;
+    // console.log(peopleData); // Print to Console
+  }).fail(function (error) {
+    console.log("Error: ", error);
+    alert("Error: ", error);
+  });
+}
 
 function initURLListener() {
   $(window).on("hashchange", changeRoute);
   changeRoute();
+  getPeopleData();
 }
 
 function initListeners() {}
@@ -17,6 +73,7 @@ $(document).ready(function () {
 });
 
 // Load Porfolio data when after document is ready
-$(window).on("load", function () {
-  // Put function to load data.
-});
+// $(window).on("load", function () {
+//   // Put function to load data.
+//   loadPeopleData();
+// });
--- src/model.js
+++ src/model.js
@@ -1,3 +1,6 @@
+// Import Functions
+import { loadPeopleData } from "./index.js";
+
 export function changeRoute() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
@@ -8,6 +11,16 @@ export function changeRoute() {
       console.log("data " + data);
       $("#app").html(data);
     });
+    if (pageID == "people") {
+      $.get(`pages/people.html`, function (data) {
+        $("#app").html(data);
+        loadPeopleData();
+      });
+      $(window).on("load", function () {
+        // Put function to load data.
+        loadPeopleData();
+      });
+    }
   } else {
     $.get(`pages/home.html`, function (data) {
       console.log("data " + data);
